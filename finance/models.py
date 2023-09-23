f25fc05addf88ba7824b0f8b9f8eb9bbc01bf1ba
Anksh S <ankitsi031999@gmail.com>
2023-09-24 00:59:55 +0530

    Create generice user and product models

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    ROLE_CHOICES = (
+        ('Advisor', 'Advisor'),
+        ('User', 'User'),
+        ('Admin', 'Admin')
+    )
+    mobile = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=255)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    category = models.CharField(max_length=255)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE, related_name='added_products')
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
