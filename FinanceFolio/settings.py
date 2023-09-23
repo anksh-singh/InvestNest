4f2db1b4d3fee4c9318c9556326fbf30ac01c99c
Anksh S <ankitsi031999@gmail.com>
2023-09-24 00:57:33 +0530

    Configure Postgres db

--- FinanceFolio/settings.py
+++ FinanceFolio/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'FinanceFolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME" : "finance_final",
+        "USER": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
+
 
 
 # Password validation
